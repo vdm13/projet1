a14e1c7aa0aa90e5c528992155310a3bc2568324
vdm13 <160516693+vdm13@users.noreply.github.com>
2026-09-11 09:26:46 +0000

    modif 12

--- program.c
+++ program.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
-int main() {
-    printf("Bonjour tout le monde!\n");
-    return 0;
+int main(void)
+{
+    printf(" ==============================================================\n");
+    printf(" |   SPD        LOC          GIS                              |\n");
+    printf(" |              ROLLOUT      FLARE                            |\n");
+    printf(" |                                                            |\n");
+    printf(" | CYY176'                    5000                            |\n");
+    printf(" | DME 1.8                    |                               |\n");
+    printf(" |                                                            |\n");
+    printf(" |        +--------------------------------------+            |\n");
+    printf(" | 200    |                                      |            |\n");
+    printf(" |        |                LAND3                 |            |\n");
+    printf(" |        |         \\        |         /         |            |\n");
+    printf(" | 180    |              ^       ^               |            |\n");
+    printf(" | 160    |           \\      |      /            |   1800     |\n");
+    printf(" |        |------------\\-----+-----/-------------|            |\n");
+    printf(" | 147    |              ----+----               |  1580      |\n");
+    printf(" |        |                  |                   |   60       |\n");
+    printf(" | 140    |                  |                   |            |\n");
+    printf(" |        |                 /|\\                  |            |\n");
+    printf(" | 120    |              --  |  --               |  1400      |\n");
+    printf(" |        |                  |                   |            |\n");
+    printf(" | 100    |                  v                   |  1200      |\n");
+    printf(" |        +--------------------------------------+            |\n");
+    printf(" |                                                            |\n");
+    printf(" | GS 150                         BARO                        |\n");
+    printf(" |                                400                         |\n");
+    printf(" |                                                            |\n");
+    printf(" |              17       18       19       20                 |\n");
+    printf(" |                       |                                    |\n");
+    printf(" |                        ----+----                                 |\n");
+    printf(" |                       ^                                    |\n");
+    printf(" ==============================================================\n");
 }
+    return 0;
